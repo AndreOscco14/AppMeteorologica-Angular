589bf7bde404d00df8eb0e5ed7f0a1c9d86aae8a
andre.oscco <andre.oscco@MacBook-Air-de-Andre.local>
2023-07-20 12:49:23 +0200

    Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,21 +10,82 @@ import { HttpClient } from '@angular/common/http';
 
 export class AppComponent implements OnInit {
   datos:any[] = []
+  datosLogMetar:any;
+  datosLogRvr: any;
 
-  constructor(private apiService: ApiService) {}
+  constructor(
+    private apiService: ApiService
+  ) {}
 
-  ngOnInit(): void {
-    this.dataFromApiFast();
+  ngOnInit(){
+    this.getData();
   }
 
-  dataFromApiFast(): void {
-    this.apiService.getApiData().subscribe((data: any) => {
-        this.datos = data;
-        console.log("Datos de FastApi",data)
-      },
-      (error) => {
-        console.error('Error al obtener datos de la API:', error);
-      }
-    );
+  async getData() {
+    try {
+      await this.dataFromApiFast();
+      await this.dataFromLogMetar();
+      await this.dataFromRvr();
+    } catch (error) {
+      console.error('Error al obtener los datos:', error);
+    }
+  }
+
+  async dataFromApiFast() {
+    try {
+      const data: any = await this.apiService.getApiData().toPromise();
+      this.datos = data;
+      console.log("Datos de FastApi", data);
+    } catch (error) {
+        console.log(error);
+    }
   }  
+
+  async dataFromLogMetar() {
+    try {
+      const datos: any = await this.apiService.getLogMetar().toPromise();
+      this.datosLogMetar = datos;
+      console.log(datos);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+ 
+  async dataFromRvr() {
+    try {
+      const data: any = await this.apiService.getLogRvr().toPromise();
+      this.datosLogRvr = data;
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
+
+//   dataFromApiFast(): void {
+//     this.apiService.getApiData().subscribe((data: any) => {
+//         this.datos = data;
+//         console.log("Datos de FastApi",data)
+//       },
+//       (error) => {
+//         console.error('Error al obtener datos de la API:', error);
+//       }
+//     );
+//   }  
+
+//   dataFromLogMetar(): void{
+//     this.apiService.getLogMetar().subscribe((datos:any) => {
+//       this.datosLogMetar = datos;
+//       console.log(datos)
+//     }, (error) => {
+//       console.error('Error con DataFromLogMetar: ', error)
+//     })
+//   }
+ 
+//   dataFromRvr() {
+//     this.apiService.getLogRvr().subscribe((data:any) => {
+//       this.datosLogRvr = data;
+//       console.log(data)
+//     })
+//   }
+// }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -13,4 +13,16 @@ export class ApiService {
     const fastApi = `${this.apiUrl}/v1/v_meteo?__limit=1&__order=-ts`; 
     return this.http.get(fastApi);
   }
+
+  getLogMetar() {
+    const logMetar= `${this.apiUrl}/v1/logmetar?__limit=100&__order=-ts`;
+    return this.http.get(logMetar)
+  }
+
+  getLogRvr() {
+    const logRvr = `${this.apiUrl}/v1/logrvrmor?__limit=100&__order=-ts`;
+    return this.http.get(logRvr)
+  }
+
+
 }
